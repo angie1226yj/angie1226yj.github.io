75efa015abfe6819da31ca76a28730ff92b04bd7
angie1226yj <127343116+angie1226yj@users.noreply.github.com>
2023-03-08 20:20:14 -0300

    v2

--- js/function.js
+++ js/function.js
@@ -11,4 +11,10 @@ function mostrarOcultarMenu(){
         menu_visible = false;
     }
 }
-
+let links = document.querySelectorAll("nav a");
+for (var x=0; x <links.length;x++){
+    links[x].onclick = function(){
+        menu.style.display = "none"
+        menu_visible = false;
+    }
+}
